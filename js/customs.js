afa31afb1dc4db54be5058b1da43b828b0607f32
pujie <puji@gmail.com>
2019-03-15 10:06:57 +0700

    add filter by quotation month

--- js/customs.js
+++ js/customs.js
@@ -14,6 +14,7 @@ gets = ()=>{
     sql+= 'a.clientaddress,'
     sql+= 'date_format(activationtarget,"%d-%b-%Y") activationtarget,'
     sql+= 'a.img,'
+    sql+= 'a.quotation_date,'
     sql+= 'b.name branch,'
     sql+= 'c.name category,'
     sql+= 'd.name service,'
@@ -23,6 +24,29 @@ gets = ()=>{
     sql+= 'left outer join categories c on c.id=a.category_id '
     sql+= 'left outer join servicenames d on d.id=a.servicename_id '
     sql+= 'left outer join medias e on e.id=a.media_id '
+    console.log("gets SQL",sql)
+    return sql
+}
+getByMonth = obj => {
+    sql = 'select a.clientname,'
+    sql+= 'a.clientpic,'
+    sql+= 'a.clienttlp,'
+    sql+= 'a.clientpichp,'
+    sql+= 'a.clientemail,'
+    sql+= 'a.clientaddress,'
+    sql+= 'date_format(activationtarget,"%d-%b-%Y") activationtarget,'
+    sql+= 'a.img,'
+    sql+= 'a.quotation_date,'
+    sql+= 'b.name branch,'
+    sql+= 'c.name category,'
+    sql+= 'd.name service,'
+    sql+= 'e.name media '
+    sql+= 'from customs a '
+    sql+= 'left outer join branches b on b.id=a.branch_id '
+    sql+= 'left outer join categories c on c.id=a.category_id '
+    sql+= 'left outer join servicenames d on d.id=a.servicename_id '
+    sql+= 'left outer join medias e on e.id=a.media_id '
+    sql+= 'where date_format(quotation_date,"MMYYYY")='+b.quotation_date+' '
     console.log("gets SQL",sql)
     return sql
 }
@@ -38,6 +62,7 @@ save = obj => {
     sql+= 'clientaddress,'
     sql+= 'activationtarget,'
     sql+= 'img,'
+    sql+= 'quotation_date,'
     sql+= 'category_id,'
     sql+= 'servicename_id,'
     sql+= 'media_id,'
@@ -54,6 +79,7 @@ save = obj => {
     sql+= '"'+obj.clientaddress+'", '
     sql+= '"'+obj.activationtarget+'", '
     sql+= '"'+obj.img+'", '
+    sql+= '"'+obj.quotation_date+'",'
     sql+= '"'+obj.category_id+'", '
     sql+= '"'+obj.servicename_id+'", '
     sql+= '"'+obj.media_id+'", '
@@ -74,6 +100,7 @@ update = obj => {
     sql+= 'clientaddress="'+obj.clientaddress+'",'
     sql+= 'activationtarget="'+obj.activationtarget+'",'
     sql+= 'img="'+obj.img+'",'
+    sql+= 'quotation_date="'+obj.quotation_date+'",'
     sql+= 'category_id="'+obj.category_id+'",'
     sql+= 'category_id="'+obj.servicename_id+'",'
     sql+= 'servicename_id="'+obj.media_id+'",'
